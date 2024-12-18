cdd3b7da989821ff278e53a490f174d5b29081e4
Parham Ebrahimi <aparhama@gmail.com>
2024-12-19 03:19:56 +0330

    create post method for register and save informations in db

--- user/views.py
+++ user/views.py
@@ -6,6 +6,7 @@ from .sms import send_sms
 from rest_framework.response import Response
 from .models import User,OTP
 from django.utils import timezone
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterView(generics.GenericAPIView,CreateModelMixin):
     serializer_class = RegisterSerializer
@@ -24,11 +25,26 @@ class RegisterView(generics.GenericAPIView,CreateModelMixin):
                     "name": "CODE",
                     "value": f"{otp_code}"
                 }
-        api='hWiijhqcGxXsSGgXNV1IrvnD0yjD9vGVeH9XwM83g8S4s9gz2Q9yOFyy0gJMPL5i'
-        send_sms(str(phone_number),608162,param,api)
+        api='hWiijhqcGxXsSGgXNV1IrvnD0yjD9vGVe'
+        send_sms(str(phone_number),608,param,api)
         print(otp_code)
         return Response({'message':'otp send successful'})
 
 
     def post(self, request, *args, **kwargs):
-        pass
+        data=request.data
+        phone_number=data.get('phone_number')
+        otp=data.get('otp')
+        if not OTP.objects.filter(phone_number=phone_number).exists():
+            return Response({'error':'otp not found for this phone number'},status.HTTP_404_NOT_FOUND)
+        otp_entry= OTP.objects.get(phone_number=phone_number)
+        if otp_entry.otp != otp or otp_entry.expires_at < timezone.now():
+            return Response({'error':'this otp invalid or expired'},status.HTTP_400_BAD_REQUEST)
+        serializer= self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        user= serializer.save()
+        otp_entry.delete()
+        refresh=RefreshToken.for_user(user)
+        return Response({'full_name':f'{user.name} {user.family}','access':str(refresh.access_token),'refresh':str(refresh),'message':'user create successful'})
+
+
