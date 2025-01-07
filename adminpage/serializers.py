702860ab6cc92d057ea9aaad5388c77dbffa6987
Parham Ebrahimi <aparhama@gmail.com>
2025-01-07 17:36:32 +0330

    create CRUD category serializer

--- adminpage/serializers.py
+++ adminpage/serializers.py
@@ -72,3 +72,12 @@ class AuctionImageListAdminSerializer(serializers.ModelSerializer):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.required = False
+
+class CategoryListAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields= '__all__'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = False
